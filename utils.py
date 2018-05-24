0b3e0232469c36f816f6f5d756c121b6391497d8
Nicolas Haderer <haderer.nicolas@gmail.com>
2018-05-25 00:09:34 +0200

    dev

---
+++ utils.py
@@ -0,0 +1,82 @@
+import numpy as np
+from pandas import concat, DataFrame
+
+
+def rnn_data(data, time_steps, labels=False):
+    """
+    creates new data frame based on previous observation
+      * example:
+        l = [1, 2, 3, 4, 5]
+        time_steps = 2
+        -> labels == False [[1, 2], [2, 3], [3, 4]]
+        -> labels == True [2, 3, 4, 5]
+    """
+    rnn_df = []
+    for i in range(len(data) - time_steps):
+        if labels:
+            try:
+                rnn_df.append(data.iloc[i + time_steps].as_matrix())
+            except AttributeError:
+                rnn_df.append(data.iloc[i + time_steps])
+        else:
+            data_ = data.iloc[i: i + time_steps].as_matrix()
+            rnn_df.append(data_ if len(data_.shape) > 1 else [[i] for i in data_])
+            return np.array(rnn_df)
+
+
+def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
+    n_vars = 1 if type(data) is list else data.shape[1]
+    df = DataFrame(data)
+    cols, names = list(), list()
+    # input sequence (t-n, ... t-1)
+    for i in range(n_in, 0, -1):
+        cols.append(df.shift(i))
+        names += [('var%d(t-%d)' % (j + 1, i)) for j in range(n_vars)]
+    # forecast sequence (t, t+1, ... t+n)
+    for i in range(0, n_out):
+        cols.append(df.shift(-i))
+        if i == 0:
+            names += [('var%d(t)' % (j + 1)) for j in range(n_vars)]
+        else:
+            names += [('var%d(t+%d)' % (j + 1, i)) for j in range(n_vars)]
+    # put it all together
+    agg = concat(cols, axis=1)
+    agg.columns = names
+    # drop rows with NaN values
+    if dropnan:
+        agg.dropna(inplace=True)
+    return agg
+
+
+def rnn_minibatch_sequencer(raw_data, batch_size, sequence_size, nb_epochs):
+    """
+    Divides the data into batches of sequences so that all the sequences in one batch
+    continue in the next batch. This is a generator that will keep returning batches
+    until the input data has been seen nb_epochs times. Sequences are continued even
+    between epochs, apart from one, the one corresponding to the end of raw_data.
+    The remainder at the end of raw_data that does not fit in an full batch is ignored.
+    :param raw_data: the training text
+    :param batch_size: the size of a training minibatch
+    :param sequence_size: the unroll size of the RNN
+    :param nb_epochs: number of epochs to train on
+    :return:
+        x: one batch of training sequences
+        y: on batch of target sequences, i.e. training sequences shifted by 1
+        epoch: the current epoch number (starting at 0)
+    """
+    data = np.array(raw_data)
+    data_len = data.shape[0]
+    # using (data_len-1) because we must provide for the sequence shifted by 1 too
+    nb_batches = (data_len - 1) // (batch_size * sequence_size)
+    assert nb_batches > 0, "Not enough data, even for a single batch. Try using a smaller batch_size."
+    rounded_data_len = nb_batches * batch_size * sequence_size
+    xdata = np.reshape(data[0:rounded_data_len], [batch_size, nb_batches * sequence_size])
+    ydata = np.reshape(data[1:rounded_data_len + 1], [batch_size, nb_batches * sequence_size])
+
+    for epoch in range(nb_epochs):
+        for batch in range(nb_batches):
+            x = xdata[:, batch * sequence_size:(batch + 1) * sequence_size]
+            y = ydata[:, batch * sequence_size:(batch + 1) * sequence_size]
+            x = np.roll(x, -epoch, axis=0)  # to continue the text from epoch to epoch (do not reset rnn state!)
+            y = np.roll(y, -epoch, axis=0)
+            yield {'x': x, 'y': y}
